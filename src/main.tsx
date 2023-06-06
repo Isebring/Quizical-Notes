1b4e7ccf66c0cd2e080439be67779f9d3aa118d5
Hampus Isebring <hampus.isebring@gmail.com>
2023-06-06 22:10:17 +0200

    Set up routing + homepage

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,28 @@
 import { MantineProvider } from '@mantine/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import App from './App';
 import './main.css';
+import Homepage from './pages/Homepage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Homepage />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <App />
+      <RouterProvider router={router} />
     </MantineProvider>
   </React.StrictMode>
 );
